e2f98424af088fbd6890eb56db33570b923aeebb
spbuda <budaspencer@gmail.com>
2014-11-27 22:40:14 -0700

    Nodes useable in a decent sense.

--- www/js/skills.js
+++ www/js/skills.js
@@ -1,17 +1,81 @@
+var instance
+
+function disableDelete(){
+	$(".node, #deleteNode").removeClass("delete");
+	$(".nodeEdit i").removeClass("fa-trash");
+	$("#addNode").show();
+};
+
+function enableDelete(){
+	$(".node, #deleteNode").addClass("delete");
+	$(".nodeEdit i").addClass("fa-trash");
+	$("#addNode").hide();
+};
+
+function toggleDelete(){
+	if($("#deleteNode").hasClass("delete"))
+		disableDelete();
+	else
+		enableDelete();
+}
+
+function removeNode(){
+	var el = this;
+	jsPlumb.detachAllConnections(el.id);
+	$(el).remove();
+	disableDelete();
+};
+
 $(document).ready(function(){
-	$(".nodeEdit").bind("click",function(){
-		var nodeText = $(this).closest(".node").find(".nodeText");
-		$("#editNodeTextModal").modal({overlayClose:true, onClose:function(){
-			$(nodeText).html($("#editNodeTextModal textarea").val());
-			$.modal.close();
-		}, onShow:function(){
-			$("#editNodeTextModal textarea").val($(nodeText).html());
-			$.modal();
-		}});
-	});
+	$("#deleteNode").on("click",toggleDelete);
 	
-	$(".nodeNumber").bind("click",function(){
-		var nodeNumber = $(this).closest(".node").find(".nodeNumber");
+	$("#plumb").on("click",".delete",removeNode);
+
+	$("#addNode").on("click",function(){
+		var t = $('#nodeTemplate')[0];
+
+		var clone = document.importNode(t.content, true);
+		var node = $("#skills #plumb").append(clone).children().last(".node");
+
+		// bind to a connection event, just for the purposes of pointing out that it can be done.
+		instance.bind("connection", function(i,c) { 
+			if (typeof console !== "undefined")
+				console.log("connection", i.connection); 
+		});
+
+		// make them draggable
+		instance.draggable($(node));
+
+		// suspend drawing and initialise.
+		instance.doWhileSuspended(function() {
+
+			// make 'window1' a connection source. notice the filter parameter: it tells jsPlumb to ignore drags
+			// that started on the 'enable/disable' link on the blue window.
+			instance.makeSource($(node).find(".nodeCreate"), {
+				//anchor:sourceAnchors,		// you could supply this if you want, but it was set in the defaults above.							
+				filter:function(evt, el) {
+					var t = evt.target || evt.srcElement;
+					return t.tagName !== "A";
+				},
+				isSource:true,
+				maxConnections:-1
+			});			
+			
+			// configure the .nodeText as targets.
+			instance.makeTarget($(node).find(".nodeText"), {
+				//anchor:"TopCenter",				// you could supply this if you want, but it was set in the defaults above.					
+				dropOptions:{ hoverClass:"hover" },
+				uniqueEndpoint:false
+			});
+		});
+
+		jsPlumb.fire("nodeCreated", instance)
+	}); 
+});
+
+function editNumber(element){
+	if(!$(element).hasClass("delete")){
+		var nodeNumber = $(element).closest(".node").find(".nodeNumber");
 		$("#editNodeNumberModal").modal({overlayClose:true, onClose:function(){
 			var str = $("#editNodeNumberModal input").val().replace(/\D+/g, "<br/>").replace(/^(<br\/>)|(<br\/>)$/g, "");
 			$(nodeNumber).html(str);
@@ -20,8 +84,21 @@ $(document).ready(function(){
 			var str = $(nodeNumber).html().replace(/\D*/g, ", ").replace(/^(, )|(, )$/g, "");
 			$("#editNodeNumberModal input").val(str);
 		}});
-	});
-});
+	}
+}
+
+function editText(element){
+	if(!$(element).hasClass("delete")){
+		var nodeText = $(element).closest(".node").find(".nodeText");
+		$("#editNodeTextModal").modal({overlayClose:true, onClose:function(){
+			$(nodeText).html($("#editNodeTextModal textarea").val());
+			$.modal.close();
+		}, onShow:function(){
+			$("#editNodeTextModal textarea").val($(nodeText).html());
+			$.modal();
+		}});
+	}
+};
 
 jsPlumb.ready(function() {
 // list of possible anchor locations for the blue source element
@@ -43,30 +120,20 @@ jsPlumb.ready(function() {
 		[ 1, 0.75, 0, 0 ]			
 	];
     
-    var instance = window.instance = jsPlumb.getInstance({
+    instance = window.instance = jsPlumb.getInstance({
     	// set default anchors.  the 'connect' calls below will pick these up, and in fact setting these means
     	// that you also do not need to supply anchor definitions to the makeSource or makeTarget functions. 
         Anchors : [ sourceAnchors, connectionAnchors ],
     	// drag options
     	DragOptions : { cursor: "pointer", zIndex:2000 },
-		// default to blue at source and green at target
-		EndpointStyles : [{ fillStyle:"#555" }, { fillStyle:"#555" }],
-		// blue endpoints 7 px; green endpoints 11.
+		EndpointStyles : [{ fillStyle:"rgba(107, 164, 94, 1)" }, { fillStyle:"rgba(221, 221, 221, 1)" }],
 		Endpoints : [ ["Dot", { radius:6 } ], [ "Dot", { radius:6 } ] ],
-		// default to a gradient stroke from blue to green.  for IE provide all green fallback.
 		PaintStyle : {
-        	strokeStyle:"#555",
+        	strokeStyle:"rgba(107, 164, 94, 1)",
         	lineWidth:3
     	},
     	Container:"plumb"
     });
-
-	// click listener for the enable/disable link.
-    /* jsPlumb.on(document.getElementById("enableDisableSource"), "click", function(e) {
-		var state = instance.toggleSourceEnabled("sourceWindow1");
-		this.innerHTML = (state ? "disable" : "enable");
-		jsPlumbUtil.consume(e);
-	}); */
 
     // bind to a connection event, just for the purposes of pointing out that it can be done.
 	instance.bind("connection", function(i,c) { 
@@ -98,10 +165,7 @@ jsPlumb.ready(function() {
 			dropOptions:{ hoverClass:"hover" },
             uniqueEndpoint:false
 		});	
-
-        // and finally connect a couple of small windows, just so its obvious what's going on when this demo loads.           
-        /* instance.connect({ source:"element1", target:"element2" }); */
 	});
 
-	jsPlumb.fire("jsPlumbDemoLoaded", instance);
+	jsPlumb.fire("jsPlumbInitialized", instance);
 });
